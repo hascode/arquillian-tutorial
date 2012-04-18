eb870b77b85332b7d7532c9fddb9f34188ae0b87
hascode <hascode@localhost>
2012-04-18 21:00:00 +0200

    JPA persistence example added.

---
+++ src/main/java/com/hascode/tutorial/jee/Book.java
@@ -0,0 +1,45 @@
+package com.hascode.tutorial.jee;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+public class Book {
+	@Id
+	@GeneratedValue
+	private Long id;
+
+	@NotNull
+	@Size(min = 3, max = 40)
+	private String title;
+
+	@NotNull
+	private String author;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(final Long id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(final String title) {
+		this.title = title;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(final String author) {
+		this.author = author;
+	}
+}
